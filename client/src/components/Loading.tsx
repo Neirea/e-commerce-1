6594746647729e6ceda80d1efbab6f2fbdadf423
neirea <neireasoultix@gmail.com>
2022-12-06 15:34:08 +0200

    added scalability to loading component

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,6 +1,10 @@
-const Loading = () => {
+import React from "react";
+const Loading = ({ size = 4 }: { size?: number }) => {
     return (
-        <div className="loading d-flex flex-column justify-content-center align-items-center">
+        <div
+            style={{ "--loading-scale": size } as React.CSSProperties}
+            className="loading d-flex flex-column justify-content-center align-items-center"
+        >
             <div className="lds-ring">
                 <div></div>
                 <div></div>
