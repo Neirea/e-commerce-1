5081a7551965bd5eec3a5614164adc8c32c5f382
neirea <neireasoultix@gmail.com>
2022-12-06 21:49:16 +0200

    added debounce to search dropdown query

--- client/src/components/Header/SearchBar.tsx
+++ client/src/components/Header/SearchBar.tsx
@@ -1,21 +1,17 @@
-import React, {
-    ChangeEvent,
-    FormEvent,
-    useEffect,
-    useRef,
-    useState,
-} from "react";
+import { useLazyQuery } from "@apollo/client";
+import { BsSearch } from "@react-icons/all-files/bs/BsSearch";
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from "react";
 import { Button, Form, InputGroup } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
-import { BsSearch } from "@react-icons/all-files/bs/BsSearch";
-import { useLazyQuery } from "@apollo/client";
-import { QUERY_SEARCH_BAR } from "../../queries/Product";
 import { GetSearchResultsQuery } from "../../generated/graphql";
+import { QUERY_SEARCH_BAR } from "../../queries/Product";
+import { useDebounce } from "../../utils/useDebounce";
 import { useOutsideClick } from "../../utils/useOutsideClick";
 import Loading from "../Loading";
 
 const SearchBar = () => {
     const [searchText, setSearchText] = useState("");
+    const debouncedText = useDebounce(searchText, 300);
     const [showResults, setShowResults] = useState(true);
     const navigate = useNavigate();
     const [getSearchResults, { data, loading, previousData }] =
@@ -25,10 +21,10 @@ const SearchBar = () => {
     const searchBarRef = useRef<HTMLInputElement>(null);
 
     const searchData = data ?? previousData;
+    const searchLoading = loading || searchText !== debouncedText;
 
     const showSearchData =
         searchData &&
-        showResults &&
         searchData.searchBarQuery.categories.length +
             searchData.searchBarQuery.companies.length +
             searchData.searchBarQuery.products.length >
@@ -36,12 +32,15 @@ const SearchBar = () => {
 
     useOutsideClick([searchBarRef], () => setShowResults(false));
 
+    useEffect(() => {
+        if (debouncedText.length && debouncedText === searchText) {
+            getSearchResults({ variables: { input: debouncedText } });
+        }
+    }, [debouncedText]);
+
     const handleSearchText = (e: ChangeEvent<HTMLInputElement>) => {
         setShowResults(true);
         setSearchText(e.target.value);
-        if (e.target.value.length) {
-            getSearchResults({ variables: { input: e.target.value } });
-        }
     };
     const search = (e: FormEvent) => {
         e.preventDefault();
@@ -72,66 +71,70 @@ const SearchBar = () => {
                 </InputGroup>
             </Form>
             {/* Search Results */}
-            <div className="position-absolute mt-3 border-bottom border-start border-end border-secondary rounded-bottom bg-white w-100">
-                {loading && (
-                    <div className="d-flex justify-content-start gap-2 p-2 border-bottom">
-                        <Loading size={1} />
-                        <span>Loading ...</span>
-                    </div>
-                )}
-                {showSearchData && (
-                    <>
-                        {searchData.searchBarQuery.categories.map((item) => {
-                            return (
-                                <Link
-                                    key={item.id}
-                                    to={`/search?c=${item.id}`}
-                                    className="text-decoration-none search-link"
-                                >
-                                    <span className="custom-link">
-                                        {item.name}
-                                    </span>
-                                    <span className="text-secondary opacity-75">
-                                        {" - Category"}
-                                    </span>
-                                </Link>
-                            );
-                        })}
-                        {searchData.searchBarQuery.companies.map((item) => {
-                            return (
-                                <Link
-                                    key={item.id}
-                                    to={`/search?b=${item.id}`}
-                                    className="text-decoration-none search-link"
-                                >
-                                    <span className="custom-link">
-                                        {item.name}
-                                    </span>
-                                    <span className="text-secondary opacity-75">
-                                        {" - Company"}
-                                    </span>
-                                </Link>
-                            );
-                        })}
-                        {searchData.searchBarQuery.products.map((item) => {
-                            return (
-                                <Link
-                                    key={item.id}
-                                    to={`/product/${item.id}`}
-                                    className="text-decoration-none search-link"
-                                >
-                                    <span className="custom-link">
-                                        {item.name}
-                                    </span>
-                                    <span className="text-secondary opacity-75">
-                                        {" - Product"}
-                                    </span>
-                                </Link>
-                            );
-                        })}
-                    </>
-                )}
-            </div>
+            {showResults && (
+                <div className="position-absolute mt-3 border-bottom border-start border-end border-secondary rounded-bottom bg-white w-100">
+                    {searchLoading && (
+                        <div className="d-flex justify-content-start gap-2 p-2 border-bottom">
+                            <Loading size={1} />
+                            <span>Loading ...</span>
+                        </div>
+                    )}
+                    {showSearchData && (
+                        <>
+                            {searchData.searchBarQuery.categories.map(
+                                (item) => {
+                                    return (
+                                        <Link
+                                            key={item.id}
+                                            to={`/search?c=${item.id}`}
+                                            className="text-decoration-none search-link"
+                                        >
+                                            <span className="custom-link">
+                                                {item.name}
+                                            </span>
+                                            <span className="text-secondary opacity-75">
+                                                {" - Category"}
+                                            </span>
+                                        </Link>
+                                    );
+                                }
+                            )}
+                            {searchData.searchBarQuery.companies.map((item) => {
+                                return (
+                                    <Link
+                                        key={item.id}
+                                        to={`/search?b=${item.id}`}
+                                        className="text-decoration-none search-link"
+                                    >
+                                        <span className="custom-link">
+                                            {item.name}
+                                        </span>
+                                        <span className="text-secondary opacity-75">
+                                            {" - Company"}
+                                        </span>
+                                    </Link>
+                                );
+                            })}
+                            {searchData.searchBarQuery.products.map((item) => {
+                                return (
+                                    <Link
+                                        key={item.id}
+                                        to={`/product/${item.id}`}
+                                        className="text-decoration-none search-link"
+                                    >
+                                        <span className="custom-link">
+                                            {item.name}
+                                        </span>
+                                        <span className="text-secondary opacity-75">
+                                            {" - Product"}
+                                        </span>
+                                    </Link>
+                                );
+                            })}
+                        </>
+                    )}
+                </div>
+            )}
         </section>
     );
 };
---
+++ client/src/utils/useDebounce.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export function useDebounce<T>(value: T, delay: number): T {
+    const [debouncedValue, setDebouncedValue] = useState<T>(value);
+    useEffect(() => {
+        // Update debounced value after delay
+        const handler = setTimeout(() => {
+            setDebouncedValue(value);
+        }, delay);
+
+        // Cancel the timeout if value changes (also on delay change or unmount)
+        return () => {
+            clearTimeout(handler);
+        };
+    }, [value, delay]);
+    return debouncedValue;
+}
