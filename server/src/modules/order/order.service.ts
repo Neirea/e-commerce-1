9fcac39f4a203d34a25d5b33822bef8f1d85fe9a
Neirea <neireasoultix@gmail.com>
2023-07-01 21:56:19 +0300

    added cron job for pending orders
    - delete pending orders every day at 4 am (record has to be more than 1 day old)
    - removed bull package and old code for it

--- server/src/modules/order/order.service.ts
+++ server/src/modules/order/order.service.ts
@@ -1,6 +1,4 @@
-import { InjectQueue } from "@nestjs/bull";
 import { Injectable } from "@nestjs/common";
-import { Queue } from "bull";
 import { PrismaService } from "../prisma/prisma.service";
 import {
     cancelOrderQuery,
@@ -12,25 +10,8 @@ import { User } from "@prisma/client";
 
 @Injectable()
 export class OrderService {
-    constructor(
-        @InjectQueue("stale-order") private staleOrderQueue: Queue,
-        private prisma: PrismaService,
-    ) {}
+    constructor(private prisma: PrismaService) {}
 
-    addOrderToQueue(orderId: OrderId): void {
-        this.staleOrderQueue.add(
-            { id: orderId },
-            {
-                delay: 24 * 60 * 60 * 1000, //24 hours
-                removeOnComplete: true,
-                attempts: 3, // If job fails it will retry 3 timess
-                backoff: {
-                    type: "exponential",
-                    delay: 60 * 60 * 1000, //retry delays: 1hour, 2hours, 4 hours
-                },
-            },
-        );
-    }
     getOrders(user: User): Promise<OrderWithItems[]> {
         const sessionUserId = user.id;
         return this.prisma.$queryRaw<OrderWithItems[]>(
