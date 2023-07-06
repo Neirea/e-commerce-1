20b6964b80d410a18a045b7a7e0da744aff16b7b
Neirea <neireasoultix@gmail.com>
2023-07-06 15:06:22 +0300

    redis ping interval

--- server/src/modules/redis/redis.service.ts
+++ server/src/modules/redis/redis.service.ts
@@ -6,7 +6,10 @@ export class RedisService {
     readonly redisClient: RedisClientType;
 
     constructor() {
-        this.redisClient = createClient({ url: process.env.REDIS_URL });
+        this.redisClient = createClient({
+            url: process.env.REDIS_URL,
+            pingInterval: 200_000,
+        });
         this.redisClient.on("error", console.error);
     }
 }
