baf50e790bc575f633f2bc6d81a28161ecf798c2
neirea <neirea@ukr.net>
2025-03-28 04:23:54 +0200

    update ecma version

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -17,7 +17,7 @@ export default tseslint.config(
                 ...globals.node,
                 ...globals.jest,
             },
-            ecmaVersion: 5,
+            ecmaVersion: "latest",
             sourceType: "module",
             parserOptions: {
                 projectService: true,
